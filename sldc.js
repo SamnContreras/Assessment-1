930add3516e34f04ddec520818ff3da7f316e441
Samuel Conteras <sammycontreras7@gmail.com>
2021-10-15 11:03:01 -0600

    Final SLDC

--- sldc.js
+++ sldc.js
@@ -12,8 +12,22 @@
 // Planning
 // Testing & Integration
 
+//      My Answer!
+// Analysis of Requirements
+// Planning
+// Design
+// Implementation
+// Testing & Intergration
+// Maintain
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+
+let analysisOfRequirements = "You'd look into the requirements or expectations of the software you are going to build"
+let planning = "You'd create a process of how you would make the software"
+let design = "You'design the software that would be displayed online"
+let implementation = "The process you go through to create the software"
+let testingAndIntergration = "You check on the website to make sure its working propelry, if not you'd fix those issues"
+let maintain = "Throughout the life of the website you'd check in to  make sure everything works properly"
